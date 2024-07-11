26c8992303337b7f1f76badde15d7b7eaadcbc3e
saiful693 <bdsaiful770@gamil.com>
2024-07-11 17:10:36 +0600

    My jobs page update and delete functionality done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Modal from 'react-modal';
 import './index.css'
 import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
+
+Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/MyJobs/MyJobs.jsx
+++ src/pages/MyJobs/MyJobs.jsx
@@ -1,11 +1,22 @@
-import { Link } from 'react-router-dom';
 import banner_img from '../../assets/all-banner.png'
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import Modal from 'react-modal';
+import DatePicker from 'react-datepicker';
+import Swal from 'sweetalert2';
+
+
 
 const MyJobs = () => {
-    const [jobs,setJobs]=useState(null);
-    const {user}=useContext(AuthContext);
+    const [jobs, setJobs] = useState(null);
+    const { user } = useContext(AuthContext);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedJob, setSelectedJob] = useState(null)
+    const [update,setUpdate]=useState(0)
+
+
+    const [job_PostingDate, setjob_PostingDate] = useState(new Date());
+    const [application_Deadline, setapplication_Deadline] = useState(new Date());
 
     function dateFormater(dateString) {
         const date = new Date(dateString);
@@ -17,16 +28,117 @@ const MyJobs = () => {
         return formattedDate;
     }
 
-    useEffect(()=>{
+
+
+    useEffect(() => {
         fetch(`http://localhost:5000/my-jobs?user_email=${user.email}`)
-        .then(res => res.json())
-        .then(data =>{
-            setJobs(data);
-        })
-    },[user.email])
+            .then(res => res.json())
+            .then(data => {
+                setJobs(data);
+            })
+            
+    }, [user.email,update])
 
-    const handleUpdate=()=>{
 
+    // modal style
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            width: '50%',
+            padding: '50px',
+            maxHeight: '80vh',
+            overflowY: 'auto',
+        },
+    };
+
+
+
+    const handleUpdateClick = async(id) => {
+        console.log(id)
+       await fetch(`http://localhost:5000/jobDetails/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                setSelectedJob(data)
+                setjob_PostingDate(new Date(data.job_PostingDate))
+                setapplication_Deadline(new Date(data.application_Deadline))
+            })
+        setIsModalOpen(true)
+   
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const job_picture = form.job_picture.value;
+        const job_title = form.job_title.value;
+        const job_category = form.job_category.value;
+        const salary_range = form.salary_range.value;
+        const job_desc = form.job_desc.value;
+        const job_Applicants = 0;
+
+        console.log(selectedJob._id)
+
+        const updated_job = { job_picture, job_title, job_category, salary_range, job_desc, job_Applicants, job_PostingDate, application_Deadline }
+
+
+        fetch(`http://localhost:5000/my-jobs/${selectedJob._id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updated_job)
+        }).then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Your Job Updated successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    });
+                    setUpdate(!update);
+                }
+            })
+    }
+
+
+    // handle delete
+    
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/jobs/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount != 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Job has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining =jobs.filter(job => job._id !==id);
+                            setJobs(remaining)
+                        }
+                    })
+
+            }
+        });
     }
 
     return (
@@ -59,11 +171,117 @@ const MyJobs = () => {
                                     <td>{dateFormater(job.job_PostingDate)}</td>
                                     <td>{job.salary_range}</td>
                                     <td><strong>{dateFormater(job.application_Deadline)}</strong></td>
-                                    <td><Link to={`/jobDetails/${job._id}`}><button onClick={handleUpdate} className="btn  bg-primary text-white">Update</button></Link></td>
+                                    <td><button onClick={() => handleUpdateClick(job._id)} className='btn btn-block text-white bg-[#28395a] hover:bg-green-400'>Update</button></td>
+                                    <td><button onClick={() => handleDelete(job._id)} className='btn btn-block text-white bg-red-400 hover:bg-red-600'>Update</button></td>
                                 </tr>)
                             }
                         </tbody>
                     </table>
+                    <Modal
+                        isOpen={isModalOpen}
+                        onRequestClose={() => setIsModalOpen(false)}
+                        style={customStyles}
+                    >
+                        <h3 className="font-bold text-2xl text-[#28395a] text-center">Update Job</h3>
+                        <form onSubmit={handleSubmit} className="card-body pb-0">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Picture URL of the Job Banner</span>
+                                </label>
+                                <input type="text" name="job_picture" defaultValue={selectedJob?.job_picture} placeholder="Job Banner Picture" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Job Title</span>
+                                </label>
+                                <input type="text" name="job_title" defaultValue={selectedJob?.job_title} placeholder="Title" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">User Name</span>
+                                </label>
+                                <input type="text" value={user.displayName} className="input input-bordered" readOnly required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">User Email</span>
+                                </label>
+                                <input type="text" value={user.email} className="input input-bordered" readOnly required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Job Category</span>
+                                </label>
+                                <select
+                                    name='job_category'
+                                    defaultValue={selectedJob?.job_category}
+                                    className="select select-bordered w-full"
+                                    required
+                                >
+                                    <option value="" disabled>Select Category</option>
+                                    <option value="On Site">On Site</option>
+                                    <option value="Remote">Remote</option>
+                                    <option value="Part-Time">Part-Time</option>
+                                    <option value="Hybrid">Hybrid</option>
+                                </select>
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Salary Range</span>
+                                </label>
+                                <input type="text" name="salary_range" defaultValue={selectedJob?.salary_range} placeholder='salary range' className="input input-bordered" required />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Job Description</span>
+                                </label>
+                                <textarea
+                                    name="job_desc"
+                                    defaultValue={selectedJob?.job_desc}
+                                    placeholder='description'
+                                    className="textarea textarea-bordered w-full"
+                                    required
+                                ></textarea>
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Job Posting date</span>
+                                </label>
+                                <DatePicker
+                                    selected={job_PostingDate}
+                                    onChange={(date) => setjob_PostingDate(date)}
+                                    className="input input-bordered w-full"
+                                    required
+                                />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Application Deadline</span>
+                                </label>
+                                <DatePicker
+                                    selected={application_Deadline}
+                                    onChange={(date) => setapplication_Deadline(date)}
+                                    className="input input-bordered w-full"
+                                    required
+                                />
+                            </div>
+
+                            <div className="form-control my-6">
+                                <button type='submit' className="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+
+
+                        <div className="modal-action">
+                            <button className='btn ' onClick={() => setIsModalOpen(false)}>Close</button>
+                        </div>
+
+                    </Modal>
+
                 </div>
             </div>
         </div>
